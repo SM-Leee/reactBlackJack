7db9a371a03a26d046000a03be4e7c67ef3449cd
tjdals2896 <tjdals2896@naver.com>
2023-07-12 16:09:24 +0900

    init

--- src/blackJack/GamePlace.js
+++ src/blackJack/GamePlace.js
@@ -6,36 +6,41 @@ import './CardList/buttonCss.css';
 const GamePlace = () => {
     const suits = ['♠', '♣', '♥', '♦'];
     const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-    const [WinPlayer, setWinPlayer] = useState(['게임을 시작해 주세요.', '']);
+    const [WinPlayer, setWinPlayer] = useState(['Please start the game.', '']);
     const [buttonBool, setButtonBool] = useState([false, true, true]);
     const [rank, setRank] = useState([0, 0]);
     const [onePlayer, setOnePlyaer] = useState([]);
     const [twoPlayer, setTwoPlyaer] = useState([]);
     const [busterCard, setBusterCard] = useState([]);
 
+    //GameStart Button
     const onHandleGameStart = () => {
-        const cardShuffle = playerCardShare(4);
-        setWinPlayer(['게임중입니다.', ''])
+        const cardShuffle = playerCardDraw(4);
+        setWinPlayer(['You are playing.', ''])
         setOnePlyaer([...onePlayer, cardShuffle[0], cardShuffle[1]]);
         setTwoPlyaer([...twoPlayer, cardShuffle[2], cardShuffle[3]]);
         setButtonBool([true, false, false]);
     }
+    //CardDraw Button
     const onHandleCardDraw = () => {
-       setTwoPlyaer([...twoPlayer, ...playerCardShare(1)]);
+       setTwoPlyaer([...twoPlayer, ...playerCardDraw(1)]);
     }
+    //Stay Button
     const onHandleStay = () => {
-        setOnePlyaer([...onePlayer, ...playerCardShare(1)]);
+        setOnePlyaer([...onePlayer, ...playerCardDraw(1)]);
         setButtonBool([true, true, true]);
     }
+    //Reset Button
     const onHandleReset = () => {
-        setWinPlayer(['게임을 시작해 주세요.', ''])
+        setWinPlayer(['Please start the game.', ''])
         setButtonBool([false, true, true])
         setRank([0, 0]);
         setOnePlyaer([]);
         setTwoPlyaer([]);
         setBusterCard([]);
     }
-    const winPlayer = (player, playerCardPack) => {
+    //카드점수계산
+    const countCardsRank = (player, playerCardPack) => {
         let sumNum = 0;
         let ARankCardCount = 0;
         for(let i = 0; i < playerCardPack.length; i++){
@@ -61,91 +66,98 @@ const GamePlace = () => {
         }
         return sumNum;
     }
-    const playerCardShare = (num) => {
+    //몇장의 카드를 Draw해줄지
+    const playerCardDraw = (num) => {
         let shareCard = [];
-        let itemCard = [];
+        let itemCard = [...busterCard];
         for(let i = 0; i < num; i ++){
-            const card = ShuffleCardValidate();
+            const card = ShuffleCardValidate(itemCard);
             shareCard = [...shareCard, card];
             itemCard = [...itemCard, card.suit + card.rank]
         }
-        setBusterCard([...busterCard, ...itemCard]);
+        setBusterCard([...itemCard]);
         return shareCard;
     }
+    //카드한장 Draw Logic
     const randomCardShuffle = () => {
         const randomSuit = Math.floor(Math.random() * suits.length);
         const randomRank = Math.floor(Math.random() * ranks.length);
         return {suit : suits[randomSuit], rank : ranks[randomRank]};
     }
-    const ShuffleCardValidate = () => {
-        const card = randomCardShuffle();
-        for(let i = 0; i < busterCard.length; i++){
-            if(busterCard[i] === card.suit + card.rank){
-                return ShuffleCardValidate();
+    //카드 나눠줄때의 Validation Check : 동일 카드가 있을때 draw를 하지않음.
+    const ShuffleCardValidate = (busterCardItems) => {
+        let card = randomCardShuffle();
+        for(let i = 0; i < busterCardItems.length; i++){
+            if(busterCardItems[i] === card.suit + card.rank){
+                return ShuffleCardValidate(busterCardItems);
             }
         }
         return card;
     }
+    //GameOver
     const GameOver = (message) => {
         setButtonBool([true, true, true]);
         setWinPlayer([message, 'GameOver']);
     }
+    //GameOver
+    const GameOverRank = (twoPlayerItem) => {
+        const oneCount = countCardsRank('1', onePlayer);
+        setRank([oneCount, twoPlayerItem]);
+    }
     useEffect(() => {
-        const twoWinPlayerCount = winPlayer('2', twoPlayer);
+        const twoWinPlayerCount = countCardsRank('2', twoPlayer);
         if(twoWinPlayerCount === 21){
-            setOnePlyaer([...onePlayer, ...playerCardShare(1)]);
+            setOnePlyaer([...onePlayer, ...playerCardDraw(1)]);
         } else if(twoWinPlayerCount > 21){
-            GameOver('1 Player 우승')
-            const oneCount = winPlayer('1', onePlayer);
-            setRank([oneCount, twoWinPlayerCount]);
+            GameOver('Dealer Winner')
+            GameOverRank(twoWinPlayerCount)
         }
     }, [twoPlayer]);
     useEffect(() => {
-        let twoPlayerCount = winPlayer('2', twoPlayer);
+        console.log(twoPlayer)
+        let twoPlayerCount = countCardsRank('2', twoPlayer);
         let onePlayerList = [...onePlayer];
         let stayBool = true;
+        let boolWin = true;
         if(WinPlayer[1] === 'GameOver'){
-            const oneCount = winPlayer('1', onePlayer);
-            setRank([oneCount, twoPlayerCount]);
+            GameOverRank(twoPlayerCount);
         }
         if(twoPlayer.length > 1 && WinPlayer[1] !== 'GameOver'){
             if(twoPlayerCount === 21){
                 stayBool = false;
-                let boolWin = true;
                 while(boolWin){
-                    let winCount = winPlayer('1', onePlayerList);
+                    let winCount = countCardsRank('1', onePlayerList);
                     if(winCount === 21){
-                        GameOver('무승부')
+                        GameOver('Draw')
                         boolWin = false;
                     }else if(winCount > 21){
-                        GameOver('2 Player 우승')
+                        GameOver('2 Player Winner')
                         boolWin = false;
                     } else {
-                        let playerCardShareAdd = playerCardShare(1);
+                        let playerCardShareAdd = playerCardDraw(1);
                         onePlayerList = [...onePlayerList, ...playerCardShareAdd];
                     }
                 }
                 setOnePlyaer(onePlayerList);
             }
             if(buttonBool[2] && stayBool){
-                let boolWin2 = true;
-                while(boolWin2){
-                    let winCount = winPlayer('1', onePlayerList);
+                while(boolWin){
+                    let winCount = countCardsRank('1', onePlayerList);
                     if(winCount === 21 && twoPlayerCount === 21){
-                        GameOver('무승부')
-                        boolWin2 = false;
+                        GameOver('Draw')
+                        boolWin = false;
                     }else if(winCount > 21 || (winCount > 16 && winCount < twoPlayerCount)){
-                        GameOver('2 Player 우승')
-                        boolWin2 = false;
+                        GameOver('2 Player Winner')
+                        boolWin = false;
                     }else if((winCount > 16 && winCount > twoPlayerCount) || winCount === 21){
-                        GameOver('1 Player 우승')
-                        boolWin2 = false;
+                        GameOver('Dealer Winner')
+                        boolWin = false;
                     } else if(winCount === twoPlayerCount && winCount > 16){
-                        GameOver('무승부')
-                        boolWin2 = false;
+                        GameOver('Draw')
+                        boolWin = false;
                     }
-                    if(boolWin2){
-                        let playerCardShareAdd = playerCardShare(1);
+                    if(boolWin){
+                        let playerCardShareAdd = playerCardDraw(1);
                         onePlayerList = [...onePlayerList, ...playerCardShareAdd];
                     }        
                 }
@@ -154,8 +166,8 @@ const GamePlace = () => {
         }
     },[onePlayer])
     useEffect(() => {
-        if(winPlayer('2', twoPlayer) === 21 && winPlayer('1', onePlayer) === 21){
-            GameOver('무승부');
+        if(countCardsRank('2', twoPlayer) === 21 && countCardsRank('1', onePlayer) === 21){
+            GameOver('Draw');
             return;
         }        
     }, [onePlayer, twoPlayer])
@@ -164,7 +176,7 @@ const GamePlace = () => {
             <hr/>
             <h1>{WinPlayer[0]}</h1>
             <hr/>
-            <h1>1 Player : {rank[0]}</h1> 
+            <h1>Dealer : {rank[0]}</h1> 
             <CardPlayer player={onePlayer}/>
             <hr/>
             <h1>2 Player : {rank[1]}</h1> 
@@ -175,7 +187,7 @@ const GamePlace = () => {
             <button className={`cardButton ${buttonBool[2] ? 'disabled' : ''}`} disabled={buttonBool[2]} onClick={onHandleStay}>Stay</button>
             <button className={`cardButton`} onClick = {onHandleReset}>reset</button>
             <hr/>
-            <a className='gameStartButton' href='/CardDeck'>Card 종류보기</a>
+            <a className='gameStartButton' href='/CardDeck'>Card List</a>
             <a className="gameStartButton" href='/'> Back </a>
         </>
         
